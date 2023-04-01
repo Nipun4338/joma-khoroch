02fd0c3973fa27e9e74316d8220408ba2b0f1433
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2023-04-01 19:54:19 +0600

    Expense api and expense list in home page added

---
+++ components/expenseCard.js
@@ -0,0 +1,52 @@
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Fragment } from "react-is";
+import { Chip } from "@mui/material";
+import { Stack } from "@mui/system";
+
+export default function ExpenseCard(props) {
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let dateLocal = new Date(props.created);
+    const createdDate = dateLocal.toLocaleDateString('en-US', options);
+    const createdTime = dateLocal.toLocaleTimeString('en-US');
+
+
+  const card = (
+    <Fragment>
+      <CardContent>
+        <Stack direction="row" justifyContent="flex-end"
+          alignItems="center">
+            {
+                props.type=="add" ? <Chip sx={{fontWeight: "bold"}} label="Added" color="success" />
+                : <Chip sx={{fontWeight: "bold"}} label="Removed" color="error" />
+            }
+        </Stack>
+        <Typography sx={{ fontSize: 18 }} color="text.secondary" gutterBottom>
+          {props.title}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {
+        props.type=="add" ? +props.attention : -props.attention}
+        </Typography>
+        <Typography sx={{ mb: 1.2, fontSize: 13 }} color="text.secondary">
+          {createdDate} at {createdTime}
+        </Typography>
+        <Typography variant="body2">{props.details}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Fragment>
+  );
+  return (
+    <>
+      <Box sx={{ minWidth: 275, marginBottom: '10px' }}>
+        <Card variant="outlined">{card}</Card>
+      </Box>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,26 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Balance from "../components/balance";
 import { Stack } from "@mui/system";
+import ExpenseCard from "../components/expenseCard";
 
 export default function Home() {
   const [balance, setBalance] = useState(0);
+  const [expenseList, setExpenseList] = useState();
 
   useEffect(() => {
     axios
-      .get("/api/get")
+      .get("/api/getbalance")
       .then((response) => {
         setBalance(response.data.rows[0]["current_balance"]);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    axios
+      .get("/api/getexpenselist")
+      .then((response) => {
+        setExpenseList(response.data.rows);
       })
       .catch((e) => {
         console.log(e);
@@ -36,6 +47,9 @@ export default function Home() {
           <Balance balance={balance} />
           <h3>/=</h3>
         </Stack>
+        {expenseList?.map((expense, i)=>{
+          return <ExpenseCard key={i} title={expense.expense_title} type={expense.expense_type}  details={expense.expense_details} attention={expense.expense} created={expense.created_date} />
+        })}
         <button
           onClick={() => {
             setBalance(balance + 8);
