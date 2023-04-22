7c3bc82f91ad484ed793463e6734ccbc679b29a3
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2023-04-22 15:23:19 +0600

    4: Insert and delete expenses added

--- components/expenseCard.js
+++ components/expenseCard.js
@@ -1,14 +1,18 @@
+import React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Fragment } from "react-is";
-import { Chip } from "@mui/material";
+import { Button, Chip, CircularProgress } from "@mui/material";
 import { Stack } from "@mui/system";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function ExpenseCard(props) {
+  const [prop, setProp] = useState(props);
+  const [loading, setLoading] = useState(true);
+
   var options = {
     weekday: "long",
     year: "numeric",
@@ -19,33 +23,78 @@ export default function ExpenseCard(props) {
   const createdDate = dateLocal.toLocaleDateString("en-US", options);
   const createdTime = dateLocal.toLocaleTimeString("en-US");
 
-  const card = (
-    <Fragment>
-      <CardContent>
-        <Stack direction="row" justifyContent="flex-end" alignItems="center">
-          {props.type == "add" ? (
-            <Chip sx={{ fontWeight: "bold" }} label="Added" color="success" />
-          ) : (
-            <Chip sx={{ fontWeight: "bold" }} label="Removed" color="error" />
-          )}
-        </Stack>
-        <Typography sx={{ fontSize: 18 }} color="text.secondary" gutterBottom>
-          {props.title}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {props.type == "add" ? +props.attention : -props.attention}
-        </Typography>
-        <Typography sx={{ mb: 1.2, fontSize: 13 }} color="text.secondary">
-          {createdDate} at {createdTime}
-        </Typography>
-        <Typography variant="body2">{props.details}</Typography>
-      </CardContent>
-    </Fragment>
-  );
+  useEffect(() => {
+    setProp(props);
+    setLoading(false);
+  }, [setProp]);
+
+  const expenseDelete = () => {
+    let data = {
+      id: prop.id,
+    };
+    axios
+      .post("/api/deleteexpense", data)
+      .then(async (response) => {
+        await props.getExpenselist(false);
+        //push("/");
+        //window.location.reload(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <>
       <Box sx={{ minWidth: 275, marginBottom: "10px" }}>
-        <Card variant="outlined">{card}</Card>
+        <Card variant="outlined">
+          <Fragment>
+            <CardContent>
+              <Stack
+                direction="row"
+                justifyContent="flex-end"
+                alignItems="center"
+              >
+                {loading ? (
+                  <CircularProgress />
+                ) : (
+                  <Chip
+                    sx={{ fontWeight: "bold" }}
+                    label={prop.type == "add" ? "Added" : "Removed"}
+                    color={prop.type == "add" ? "success" : "error"}
+                  />
+                )}
+              </Stack>
+              <Typography
+                sx={{ fontSize: 18 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {prop.title}
+              </Typography>
+              <Typography variant="h5" component="div">
+                ৳. {prop.attention}
+              </Typography>
+              <Typography sx={{ mb: 1.2, fontSize: 13 }} color="text.secondary">
+                {createdDate} at {createdTime}
+              </Typography>
+              <Typography variant="body2">{prop.details}</Typography>
+              <Stack
+                direction="row"
+                justifyContent="flex-end"
+                alignItems="center"
+              >
+                <Button
+                  onClick={expenseDelete}
+                  variant="contained"
+                  sx={{ fontWeight: "bold", backgroundColor: "red" }}
+                >
+                  DELETE
+                </Button>
+              </Stack>
+            </CardContent>
+          </Fragment>
+        </Card>
       </Box>
     </>
   );
--- pages/api/getexpenselist/index.js
+++ pages/api/getexpenselist/index.js
@@ -3,11 +3,11 @@ import conn from "../../../lib/db";
 export default async (req, res) => {
   try {
     //console.log("req nom", req.body);
-    const query = "SELECT expense_id, expense_title, expense_details, created_date, updated_date, status, expense from expenses order by created_date desc";
+    const query = "SELECT expense_id, expense_title, expense_details, created_date, updated_date, status, expense, expense_type from expenses order by created_date desc";
     //const values = [req.body.content];
     const result = await conn.query(query);
     //console.log("ttt", result);
-    res.send(result);
+    res.status(200).json(result);
   } catch (error) {
     //console.log(error);
   }
