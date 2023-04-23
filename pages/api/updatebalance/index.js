b366ae5b2886675da94d35238c728f53d1ff3eca
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2023-04-23 11:29:43 +0600

    5: Double click edit balance added

--- pages/api/updatebalance/index.js
+++ pages/api/updatebalance/index.js
@@ -2,9 +2,8 @@ import conn from "../../../lib/db";
 
 export default async (req, res) => {
   try {
-    const query = "DELETE FROM expenses where expense_id = $1";
-    const values = [req.body.id];
-    console.log(req.body);
+    const query = "UPDATE balance SET current_balance = $1";
+    const values = [req.body.balance];
     const result = await conn.query(query, values);
     res.send(result);
   } catch (error) {
